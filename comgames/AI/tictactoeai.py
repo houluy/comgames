225859f89d70f751b6e508e6815cdd9fa4d3b80e
houluy <houlu8674@bupt.edu.cn>
2020-06-25 00:33:22 +0800

    add two agents to combat

--- comgames/AI/tictactoeai.py
+++ comgames/AI/tictactoeai.py
@@ -1,6 +1,13 @@
 import random
 from collections import defaultdict, UserDict
 from collections.abc import Iterable, MutableSequence
+import logging
+import pickle
+
+fmt = "%(message)s"
+logging.basicConfig(
+    level=logging.DEBUG, format=fmt, filename="logs.log"
+)
 
 import comgames.game
 
@@ -11,9 +18,8 @@ class Env:
         self.game.board.print_pos()
         self.max_round = self.game.board.mround()
         self.reward_dic = {
-            1: 10,
-            -1: -10,
-            -2: 5, # Duel
+            1: 100,
+            -2: 0, # Duel
             0: 0, 
         }
 
@@ -44,28 +50,14 @@ class Env:
             return self.game.board.state, self._reward(1), done, "Agent wins!"
         if not finish:
             self.game.board.game_round += 1
-            pos = self.game.input_pos()
-            self.game.board.set_pos(pos)
-            self.game.board.print_pos(coordinates=[pos])
-            finish = self.game.board.check_win_by_step(pos, player=self.game.board.player)
-            if self.game.board.game_round == self.max_round and not finish:
-                done = True
-                self.game.celebrate(duel=True)
-                return self.game.board.state, self._reward(-2), done, "Duel!"
-            if finish:
-                done = True
-                self.game.celebrate(duel=False)
-                return self.game.board.state, self._reward(-1), done, "Player wins!"
-            if not finish:
-                self.game.board.game_round += 1
-                return self.game.board.state, self._reward(0), done, "Not finished"
+            return self.game.board.state, self._reward(0), done, "Switch player!"
 
     def reset(self):
         self.game.board.clear()
 
 
 class Q(UserDict):
-    def __init__(self, gamma=0.99, alpha=0.1):
+    def __init__(self, gamma=0.9, alpha=0.1):
         self.gamma = gamma
         self.alpha = alpha
         super().__init__()
@@ -124,6 +116,12 @@ class Q(UserDict):
             max_nQ = self[(next_state,)]
         self[key] = current_Q + self.alpha*(reward + self.gamma * max_nQ - current_Q)
 
+    def sum(self):
+        val = 0
+        for k, v in self.data.items():
+            val += v
+        return val
+
 
 class Agent:
     def __init__(self):
@@ -152,22 +150,52 @@ class Agent:
 
 
 def main():
-    episodes = 5
+    episodes = 3000
     env = Env()
-    agent = Agent()
+    agent_off = Agent()
+    agent_def = Agent()
 
     for e in range(episodes):
         state = env.observation()
         done = False
+        game_round = 0
         while not done:
+            game_round += 1
             actions = env.actions(state)
-            action = agent.epsilon_greedy(state, actions)
-            next_state, reward, done, info = env.step(action)
-            if done:
-                agent.Q.update(state, action, next_state, reward) 
-            else:
-                agent.Q.update(state, action, next_state, reward, actions=env.actions(next_state))
-                state = next_state
+            action_off = agent_off.epsilon_greedy(state, actions)
+            intermediate_state, reward_off, done, info = env.step(action_off)
+            if done: # offensive agent wins or duel
+                reward_def = -reward_off
+                agent_off.Q.update(state, action_off, intermediate_state, reward_off) 
+                agent_def.Q.update(last_inter_state, action_def, intermediate_state, reward_def)
+            else: # turn of defensive agent
+                actions = env.actions(intermediate_state)
+                reward_def = reward_off
+                # Need to udpate the Q value of defensive agent after the first round
+                if game_round > 1:
+                    agent_def.Q.update(last_inter_state, action_def, intermediate_state, reward_def, actions)
+                game_round += 1
+                action_def = agent_def.epsilon_greedy(intermediate_state, actions)
+                next_state, reward_def, done, info = env.step(action_def)
+                if done: # defensive agent wins or duel
+                    reward_off = -reward_def
+                    agent_def.Q.update(intermediate_state, action_def, next_state, reward_def)
+                    agent_off.Q.update(state, action_off, next_state, reward_off)
+                else: 
+                    actions = env.actions(next_state)
+                    agent_off.Q.update(state, action_off, next_state, reward_off, actions)
+                    last_inter_state = intermediate_state[:]
+            logging.debug(f"Offensive: state:{state}, action:{action_off}, reward:{reward_off}, next_state: {intermediate_state}")
+            logging.debug(f"Defensive: state:{intermediate_state}, action:{action_def}, reward:{reward_def}, next_state: {next_state}")
+            logging.debug(f"Offensive Q sum: {agent_off.Q.sum()}, Defensive Q sum: {agent_def.Q.sum()}")
+            state = next_state[:]
+
         env.reset()
-    print(agent.Q)
+    with open("Q/offensive.pkl", "wb") as f:
+        pickle.dump(agent_off.Q, f)
+    with open("Q/defensive.pkl", "wb") as f:
+        pickle.dump(agent_def.Q, f)
+#    print(agent_off.Q)
+#    print(agent_def.Q)
+#    print(game_round)
 
