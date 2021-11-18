fd55b566f3f3e6b5badcb63fc9b419a2ef7e6a7a
houluy <houlu8674@bupt.edu.cn>
2021-11-18 11:04:13 +0800

    playable

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
-fmtter = logging.Formatter("%(name)s--%(levelname)s: %(message)s")
-handler.setFormatter(fmtter)
-logger = logging.getLogger(__name__)
-logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
